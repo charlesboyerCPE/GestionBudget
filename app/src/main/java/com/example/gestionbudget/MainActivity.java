c5021e5954f0d5ec28f1d6bb43f0b7d1e23ae04b
Atma <charles.boyer021@gmail.com>
2020-08-10 23:59:34 +0200

    Coding the activity

--- app/src/main/java/com/example/gestionbudget/MainActivity.java
+++ app/src/main/java/com/example/gestionbudget/MainActivity.java
@@ -1,23 +1,58 @@
 package com.example.gestionbudget;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText mTotalBudget,
+        mFood,
+        mGoOut,
+        mHouseHold,
+        mClothes,
+        mTransport,
+        mRent,
+        mSalary;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTotalBudget =  findViewById(R.id.editTextNumberDecimalTotalBudget);
+        mFood        =  findViewById(R.id.editTextNumberDecimalFood);
+        mGoOut       =  findViewById(R.id.editTextNumberDecimalGoOut);
+        mHouseHold   =  findViewById(R.id.editTextNumberDecimalHousehold);
+        mClothes     =  findViewById(R.id.editTextNumberDecimalClothes);
+        mTransport   =  findViewById(R.id.editTextNumberDecimalTransport);
+        mRent        =  findViewById(R.id.editTextNumberDecimalRent);
+        mSalary      =  findViewById(R.id.editTextNumberDecimalSalary);
     }
 
     public void onClickConfirmed(View view)
     {
+        if(isEmpty(mTotalBudget) || isEmpty(mFood) || isEmpty(mGoOut) || isEmpty())
+    }
 
+    public void generateDialogue(String title, String message)
+    {
+        AlertDialog.Builder messageAlerte;
+
+        messageAlerte = new AlertDialog.Builder(this);
+        messageAlerte.setTitle(title);
+        messageAlerte.setMessage(message);
+        messageAlerte.setPositiveButton(R.string.buttonDialogue, null);
+        messageAlerte.create().show();
+    }
+
+    public boolean isEmpty (EditText editText)
+    {
+        return TextUtils.isEmpty(editText.getText().toString());
     }
 }
